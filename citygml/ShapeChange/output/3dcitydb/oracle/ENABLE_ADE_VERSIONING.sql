d9660139c008d32ec37a1ca5a1a748cf3791c940
F Rehmann <49240249+c0nb4@users.noreply.github.com>
2025-06-19 11:42:16 +0200

    Update: ICT Data model

---
+++ citygml/ShapeChange/output/3dcitydb/oracle/ENABLE_ADE_VERSIONING.sql
@@ -0,0 +1,6 @@
+-- This document was automatically created by the ADE-Manager tool of 3DCityDB (https://www.3dcitydb.org) on 2025-06-19 11:23:13 
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+-- *********************************** Enable Versioning ********************************** 
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+
+exec DBMS_WM.EnableVersioning('sri_assessor,sri_assetdata,sri_building,sri_communicationprotocol,sri_controllogic,sri_cyberdevicedata,sri_dataconnector,sri_datasource,sri_designbasisdata,sri_energydata,sri_functionalitylevel,sri_ictequipment,sri_indoorenvironmentalda,sri_informationneed,sri_informationneeddataca,sri_interface,sri_model,sri_occupantdata,sri_onsiteenergygeneratio,sri_operationaldata,sri_outdoorenvironmentald,sri_servicecatalogue,sri_sriassessment,sri_sriservice,sri_supportedaccess,sri_utilitygriddata','VIEW_WO_OVERWRITE');
